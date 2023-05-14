9ca1a9c16beb02ac61654eb353339ef7a4b003a0
aliaouidet <alitunisi9@gmail.com>
2023-05-14 23:01:31 +0100

    s

--- ArtyProd/service/models.py
+++ ArtyProd/service/models.py
@@ -8,33 +8,32 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 #python manage.py migrate
 
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     photo = models.ImageField(upload_to='user_photos', null=True, blank=True)
-    email = models.EmailField()
     phone = models.CharField(max_length=15)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='custom_user_groups',
-        related_query_name='custom_user'
-    )
-
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='custom_user_permissions',
-        related_query_name='custom_user'
-    )
+    objects = CustomUserManager()
 
     def __str__(self):
-        return f"{self.last_name}, {self.first_name}"
+        return self.email
+
+
+
+
 class Project(models.Model):
     STATUS_CHOICES = (
         ('IP', 'In Progress'),
